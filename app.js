d27711146ebf7985d160c52a83d909a7401c24c1
Collins Kariuki <collinsmunkary@gmail.com>
2022-07-27 11:36:23 +0300

    creating and accessing methods using dot and square bracket notation

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 //Object literals
+
+let user = {
+    name: 'crystal',
+    age: 30,
+    email: 'cystal@thenetninja.co.uk',
+    location: 'berlin',
+    blogs: ['why mac & cheese rules', '10 things to make with marmite']
+};
+
+console.log(user);
+console.log(user.name);
+
+//update a property on an object
+user.age = 35;
+console.log(user.age);
+
+//another way of accessing object properties
+//square braacket notation
+console.log(user['email']);
+user['name'] = 'chun-li';
+console.log(user['name'])
