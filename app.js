bb80f0dfb01d90b84a354ada5916ccaf52b4b573
Collins Kariuki <collinsmunkary@gmail.com>
2022-07-28 06:54:43 +0300

    Reference values

--- app.js
+++ app.js
@@ -58,19 +58,29 @@
 
 //inbuilt methods in javascript
 //Math object
-console.log(Math);
-console.log(Math.PI);
-console.log(Math.E);
+// console.log(Math);
+// console.log(Math.PI);
+// console.log(Math.E);
 
-const area = 7.7;
-console.log(Math.round(area));
-console.log(Math.floor(area));
-console.log(Math.ceil(area));
-console.log(Math.trunc(area));
+// const area = 7.7;
+// console.log(Math.round(area));
+// console.log(Math.floor(area));
+// console.log(Math.ceil(area));
+// console.log(Math.trunc(area));
 
 
 //random numbers
-const random = Math.random();
+// const random = Math.random();
 
-console.log(random);
-console.log(Math.round(random * 10))
+// console.log(random);
+// console.log(Math.round(random * 10))
+
+
+
+//primitive values
+let scoreOne = 50;
+let scoreTwo = scoreOne;
+console.log(`scoreOne: ${scoreOne} scoreTwo: ${scoreTwo}`);
+
+scoreOne = 100;
+console.log(`scoreOne: ${scoreOne} scoreTwo: ${scoreTwo}`);
