351ed34a78b056496b436114eb9b45f886428fea
Collins Kariuki <collinsmunkary@gmail.com>
2022-07-28 06:14:04 +0300

    JS inbuilt objects, Math Object

--- app.js
+++ app.js
@@ -8,48 +8,69 @@
 
 // console.log(blogs);
 
-let user = {
-    name: 'crystal',
-    age: 30,
-    email: 'cystal@thenetninja.co.uk',
-    location: 'berlin',
-    blogs: [
-        {title: 'why mac & cheese rules', likes: 30},
-        {title: '10 things to make with marmite', likes: 50}
-    ],
-    //the name of this method is login, and the value is what it does
-    login(){
-        console.log("the user logged in");
-    },
-    logout(){
-        console.log("the user logged out");
-    },
-    logBlogs(){
-        console.log(this.blogs);
-        console.log('this user has written the following blogs:');
-        this.blogs.forEach(blog => {
-            console.log(blog.title, blog.likes);
-        })
-    }
-};
+// let user = {
+//     name: 'crystal',
+//     age: 30,
+//     email: 'cystal@thenetninja.co.uk',
+//     location: 'berlin',
+//     blogs: [
+//         {title: 'why mac & cheese rules', likes: 30},
+//         {title: '10 things to make with marmite', likes: 50}
+//     ],
+//     //the name of this method is login, and the value is what it does
+//     login(){
+//         console.log("the user logged in");
+//     },
+//     logout(){
+//         console.log("the user logged out");
+//     },
+//     logBlogs(){
+//         console.log(this.blogs);
+//         console.log('this user has written the following blogs:');
+//         this.blogs.forEach(blog => {
+//             console.log(blog.title, blog.likes);
+//         })
+//     }
+// };
 
-console.log(user);
-console.log(user.name);
+// console.log(user);
+// console.log(user.name);
 
 //update a property on an object
-user.age = 35;
-console.log(user.age);
+// user.age = 35;
+// console.log(user.age);
 
 //another way of accessing object properties
 //square braacket notation
-console.log(user['email']);
-user['name'] = 'chun-li';
-console.log(user['name']);
+// console.log(user['email']);
+// user['name'] = 'chun-li';
+// console.log(user['name']);
 
 //call the method described in the object
-user.login();
-user.logout();
+// user.login();
+// user.logout();
 
 //using this in a global context which is the window object
 // console.log(this);
-user.logBlogs();
+// user.logBlogs();
+
+
+
+//inbuilt methods in javascript
+//Math object
+console.log(Math);
+console.log(Math.PI);
+console.log(Math.E);
+
+const area = 7.7;
+console.log(Math.round(area));
+console.log(Math.floor(area));
+console.log(Math.ceil(area));
+console.log(Math.trunc(area));
+
+
+//random numbers
+const random = Math.random();
+
+console.log(random);
+console.log(Math.round(random * 10))
