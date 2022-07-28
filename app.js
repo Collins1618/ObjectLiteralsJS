86d9e3264bdbde079199ed022718f9ab377b559c
Collins Kariuki <collinsmunkary@gmail.com>
2022-07-28 05:43:03 +0300

    using the this keyword in methods to refer to the object itself

--- app.js
+++ app.js
@@ -5,7 +5,21 @@ let user = {
     age: 30,
     email: 'cystal@thenetninja.co.uk',
     location: 'berlin',
-    blogs: ['why mac & cheese rules', '10 things to make with marmite']
+    blogs: ['why mac & cheese rules', '10 things to make with marmite'],
+    //the name of this method is login, and the value is what it does
+    login: function(){
+        console.log("the user logged in");
+    },
+    logout: function(){
+        console.log("the user logged out");
+    },
+    logBlogs: function(){
+        console.log(this.blogs);
+        console.log('this user has written the following blogs:');
+        this.blogs.forEach(blog => {
+            console.log(blog);
+        })
+    }
 };
 
 console.log(user);
@@ -19,4 +33,12 @@ console.log(user.age);
 //square braacket notation
 console.log(user['email']);
 user['name'] = 'chun-li';
-console.log(user['name'])
+console.log(user['name']);
+
+//call the method described in the object
+user.login();
+user.logout();
+
+//using this in a global context which is the window object
+// console.log(this);
+user.logBlogs();
